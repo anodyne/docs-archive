1b9ad963743215acab5a6694df9756e834eead4d
agentphoenix <david.vanscott@gmail.com>
2011-05-06 08:42:05 -0400

    updating to kohana 3.1.3

--- application/logs/2011/02/18.php
+++
@@ -1,8 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2011-02-18 13:44:17 --- ERROR: Http_Exception_404 [ 404 ]: The requested URL start/index was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 94 ]
-2011-02-18 13:45:00 --- ERROR: Http_Exception_404 [ 404 ]: The requested URL start/index was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 120 ]
-2011-02-18 13:45:59 --- ERROR: Http_Exception_404 [ 404 ]: The requested URL start/index was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 120 ]
-2011-02-18 13:46:11 --- ERROR: Http_Exception_404 [ 404 ]: The requested URL start/index was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 120 ]
-2011-02-18 13:51:17 --- ERROR: Http_Exception_404 [ 404 ]: The requested URL nova2/index was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 94 ]
-2011-02-18 13:56:06 --- ERROR: Http_Exception_404 [ 404 ]: Unable to find a route to match the URI: application/views/design/style.css ~ SYSPATH\classes\kohana\request.php [ 733 ]
