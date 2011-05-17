474c67cd3c9bb251efd2713c5e0f979a1d253538
agentphoenix <david.vanscott@gmail.com>
2011-05-17 14:37:04 -0400

    content updates

--- application/views/components/pages/main.php
+++ application/views/components/pages/main.php
@@ -4,15 +4,15 @@
 
 <a href="<?php echo Url::site('nova2/overview/index');?>" class="options-noicons">
 	<span>Nova 2</span>
-	<em>Nova 2 is the continued evolution of Anodyne's new flagship product. Utilizing a brand new, PHP 5 only system core, Nova 2 brings a new level of advancement "under the hood" and wide variety of tweaks to the system to make signficant improvements to Nova 1. This exhaustive user guide is your one-stop-shop to all things Nova 2. <strong>Note:</strong> Nova 2 is still under heavy development and constantly changing, so all material in this section should be considered beta and subject to change before Nova 2's final release.</em>
+	<em>Nova 2 is the continued evolution of Anodyne's flagship product. Utilizing a revamped system core (PHP 5.1 and higher), Nova 2 brings a wide array of enhancements to Nova and streamlines the update process, making Nova 2 the easiest system to install and update that we've ever released. This user guide is your one-stop-shop to all things Nova 2.</em>
 </a>
 
 <a href="<?php echo Url::site('nova1/overview/index');?>" class="options-noicons">
 	<span>Nova 1</span>
-	<em>Nova is the evolution of years of Anodyne's work in RPG management. Taking cues from SMS 2, Nova brings a great level of power, flexibility and control to increasingly easy to use system. Complete with several major features and a host of smaller gems, Nova is the ultimate RPG management tool. This exhaustive user guide is your one-stop-shop to all things Nova.</em>
+	<em>Released in 2010, Nova is the evolution of years of Anodyne's work in RPG management. Taking cues from SMS 2, Nova brings a new level of power, flexibility and control to make the system easier to use than ever before. Complete with several major features and a host of smaller gems, Nova is the ultimate RPG management tool. This user guide is your one-stop-shop to all things Nova.</em>
 </a>
 
 <a href="<?php echo Url::site('sms/overview/index');?>" class="options-noicons">
 	<span>SMS</span>
-	<em>In 2005, Anodyne took the simming world by storm with the release of SMS 1.2, an easy to use RPG management tool that catapulted us to the forefront of RPG management. Over the course of 3 years, Anodyne continued to improve and update SMS to make it the premier RPG management tool. This limited user guide will answer commonly asked questions and provide some tutorials for working with SMS.</em>
+	<em>In 2005, Anodyne took the simming world by storm with the release of SMS 1.2, an easy to use RPG management tool that catapulted Anodyne to the forefront of RPG management. Over the course of 3 years, Anodyne continued to improve and update SMS to make it the premier RPG management tool. This limited user guide will answer commonly asked questions and provide some tutorials for working with SMS.</em>
 </a>
