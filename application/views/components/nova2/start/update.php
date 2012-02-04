82bb536290f43b197e5e90eceebad4a3f85b6357
David VanScott <david.vanscott@gmail.com>
2012-02-03 21:19:56 -0500

    Update the Nova 2 docs

--- application/views/components/nova2/start/update.php
+++ application/views/components/nova2/start/update.php
@@ -12,22 +12,18 @@
 
 <p>Use the update guides below for detailed instructions on how to update from one version to another. If you're running Nova 2 and want to update across multiple versions, you'll just need to remove the <samp>nova</samp> directory and upload a new copy. If you're running Nova 1 and want to update across multiple versions, you'll need to go through each update guide from the version you're on to the version you're going to (skipping the step where you run the update from the browser) and then after all the files are updated, do the system update from the browser.</p>
 
-<ul>
-	<li><a href="<?php echo Url::site('nova2/start/update/126_to_200');?>">Nova 1.2.6 to Nova 2.0</a></li>
-	<li><a href="<?php echo Url::site('nova2/start/update/125_to_126');?>">Nova 1.2.5 to Nova 1.2.6</a></li>
-	<li><a href="<?php echo Url::site('nova2/start/update/124_to_125');?>">Nova 1.2.4 to Nova 1.2.5</a></li>
-	<li><a href="<?php echo Url::site('nova2/start/update/123_to_124');?>">Nova 1.2.3 to Nova 1.2.4</a></li>
-	<li><a href="<?php echo Url::site('nova2/start/update/122_to_123');?>">Nova 1.2.2 to Nova 1.2.3</a></li>
-	<li><a href="<?php echo Url::site('nova2/start/update/121_to_122');?>">Nova 1.2.1 to Nova 1.2.2</a></li>
-	<li><a href="<?php echo Url::site('nova2/start/update/120_to_121');?>">Nova 1.2 to Nova 1.2.1</a></li>
-	<li><a href="<?php echo Url::site('nova2/start/update/112_to_120');?>">Nova 1.1.2 to Nova 1.2</a></li>
-	<li><a href="<?php echo Url::site('nova2/start/update/111_to_112');?>">Nova 1.1.1 to Nova 1.1.2</a></li>
-	<li><a href="<?php echo Url::site('nova2/start/update/110_to_111');?>">Nova 1.1 to Nova 1.1.1</a></li>
-	<li><a href="<?php echo Url::site('nova2/start/update/106_to_110');?>">Nova 1.0.6 to Nova 1.1</a></li>
-	<li><a href="<?php echo Url::site('nova2/start/update/105_to_106');?>">Nova 1.0.5 to Nova 1.0.6</a></li>
-	<li><a href="<?php echo Url::site('nova2/start/update/104_to_105');?>">Nova 1.0.4 to Nova 1.0.5</a></li>
-	<li><a href="<?php echo Url::site('nova2/start/update/103_to_104');?>">Nova 1.0.3 to Nova 1.0.4</a></li>
-	<li><a href="<?php echo Url::site('nova2/start/update/102_to_103');?>">Nova 1.0.2 to Nova 1.0.3</a></li>
-	<li><a href="<?php echo Url::site('nova2/start/update/101_to_102');?>">Nova 1.0.1 to Nova 1.0.2</a></li>
-	<li><a href="<?php echo Url::site('nova2/start/update/100_to_101');?>">Nova 1.0 to Nova 1.0.1</a></li>
-</ul>
+<p class="alert alert-info"><strong>Note:</strong> The update guides assume you're coming from the immediately prior version. So if you're reading the Nova 2.0.4 guide, it's assuming you're coming from version 2.0.3, not 2.0.1 (or earlier). If you are several updates behind, you can simply delete the <strong>nova</strong> directory, replace it with the newest version, and then run the update script in the browser.</p>
+<br>
+
+<table class="table table-striped table-bordered">
+	<thead>
+		<th class="blue span2">Version</th>
+		<th class="blue">Changes</th>
+	</thead>
+	<tbody>
+		<tr>
+			<td><a href="<?php echo Url::site('nova2/start/update/126_to_200');?>">Nova 2.0</a></td>
+			<td>You've spoken and we've listened. The feedback we've consistently heard about Nova is that it's great, but there are some things that could be better. Instead of waiting for the next generation of Nova, we've decided to address some of theses issues now. Think Nova is difficult to update? We've fixed that with a brand-new file structure that makes updating as easy as deleting a folder and uploading a new copy of that folder. Think Thresher is a bit limited? We've addressed that with enhancements to just about every part of the wiki (all-new category selection UI, creating categories while creating/editing pages, page restrictions, the ability to edit the content of core wiki pages, better UIs for several pages, etc.). Frustrated by editing a post only to have it wiped out because someone was editing the post at the same time? No more with a brand-new post locking feature that makes sure those frustrations are a thing of the past. On top of that, we've added or enhanced a ton of features in Nova 2 (users will be automatically removed from a post if they didn't participate in it, add previous disallowed HTML content in site messages like script tags and embedded videos, simplified user and character management controls, all-new character selection mechanism for posts and messages, private messaging updates, a new look and feel for Pulsar and Titan, Writing Control Panel improvements, and much more). No matter what way you slice, Nova 2 is a big step forward.</td>
+		</tr>
+	</tbody>
+</table>
