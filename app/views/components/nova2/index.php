57dcd5b42be4024de30d68d533340efac8ec5368
David VanScott <david.vanscott@gmail.com>
2012-06-18 08:55:29 -0400

    Update the Nova 2 main page layout

--- app/views/components/nova2/index.php
+++ app/views/components/nova2/index.php
@@ -1,37 +1,30 @@
 <h1>Nova 2</h1>
-<br>
 
-<ul class="thumbnails">
-	<li class="span4">
-		<div class="thumbnail">
-			<div class="caption">
-				<img src="<?php echo Uri::base(false);?>app/views/design/images/nova2/getting-started.png" alt="Getting Started">
-				<h2>Getting Started</h2>
-				<p>Get up and running with Nova 2 in no time with tutorials for doing a fresh install, upgrading from SMS, updating from Nova 1 and more!</p>
-				<p><a href="<?php echo Uri::create('nova2/start/index');?>" class="btn">Get Started</a></p>
-			</div>
+<div class="row">
+	<div class="span4">
+		<div class="well">
+			<img src="<?php echo Uri::base(false);?>app/views/design/images/nova2/getting-started.png" alt="Getting Started">
+			<h2>Getting Started</h2>
+			<p>Get up and running with Nova 2 in no time with tutorials for doing a fresh install, upgrading from SMS, updating from Nova 1 and more!</p>
+			<p><a href="<?php echo Uri::create('nova2/start/index');?>" class="btn">Get Started</a></p>
 		</div>
-	</li>
+	</div>
 	
-	<li class="span4">
-		<div class="thumbnail">
+	<div class="span4">
+		<div class="well">
 			<img src="<?php echo Uri::base(false);?>app/views/design/images/nova2/kitchen-sink.png" alt="Kitchen Sink">
-			<div class="caption">
-				<h2>The Kitchen Sink</h2>
-				<p>Have questions about Nova 2? Need information about the ins and outs of the system? Everything else you'll need to know about Nova 2 is here.</p>
-				<p><a href="<?php echo Uri::create('nova2/everything/index');?>" class="btn">Dig In</a></p>
-			</div>
+			<h2>The Kitchen Sink</h2>
+			<p>Have questions about Nova 2? Need information about the ins and outs of the system? Everything else you'll need to know about Nova 2 is here.</p>
+			<p><a href="<?php echo Uri::create('nova2/everything/index');?>" class="btn">Dig In</a></p>
 		</div>
-	</li>
+	</div>
 	
-	<li class="span4">
-		<div class="thumbnail">
-			<div class="caption">
-				<img src="<?php echo Uri::base(false);?>app/views/design/images/nova2/develop.png" alt="Developers">
-				<h2>Developers</h2>
-				<p>Skinning, modifications and advanced tutorials are just a few ways developers can extend Nova 2 and make it unique for your own game.</p>
-				<p><a href="<?php echo Uri::create('nova2/developers/index');?>" class="btn">Developer Resources</a></p>
-			</div>
+	<div class="span4">
+		<div class="well">
+			<img src="<?php echo Uri::base(false);?>app/views/design/images/nova2/develop.png" alt="Developers">
+			<h2>Developers</h2>
+			<p>Skinning, modifications and advanced tutorials are just a few ways developers can extend Nova 2 and make it unique for your own game.</p>
+			<p><a href="<?php echo Uri::create('nova2/developers/index');?>" class="btn">Developer Resources</a></p>
 		</div>
-	</li>
-</ul>
+	</div>
+</div>
