ce1c0f269db04e0efdbd0037742c8ac813eef7fe
David VanScott <david.vanscott@gmail.com>
2012-10-04 15:17:01 -0400

    Add Nova 3 components page

---
+++ app/views/components/nova3/everything/components.php
@@ -0,0 +1,134 @@
+<h1>Nova Components</h1>
+<br>
+
+<table class="table table-striped table-bordered">
+	<tbody>
+		<tr>
+			<td class="span3">
+				<strong><a href="http://codeigniter.com/" target="_blank">CodeIgniter</a></strong><br>
+				<span class="muted">2.1.2</span>
+			</td>
+			<td>CodeIgniter is an open source web application framework for use in building dynamic web sites with PHP. It enables developers to build applications faster - compared to coding from scratch - by providing a rich set of libraries for commonly needed tasks, as well as a simple interface and a logical structure to access these libraries.</td>
+		</tr>
+		<tr>
+			<td>
+				<strong>Thresher</strong><br>
+				<span class="muted">Release 2</span>
+			</td>
+			<td>Thresher is Anodyne Productions' integrated mini-wiki for Nova.</td>
+		</tr>
+		<tr>
+			<td>
+				<strong><a href="http://codeigniter.com/forums/viewthread/67028/" target="_blank">Template Library</a></strong>
+			</td>
+			<td>Simple template engine designed by wiredesignz to work with Modular CI.</td>
+		</tr>
+		<tr>
+			<td>
+				<strong><a href="http://www.jquery.com" target="_blank">jQuery</a></strong><br>
+				<span class="muted">1.7.2</span>
+			</td>
+			<td>jQuery is a lightweight JavaScript library that emphasizes interaction between JavaScript and HTML.</td>
+		</tr>
+		<tr>
+			<td>
+				<strong><a href="http://www.jqueryui.com" target="_blank">jQuery UI</a></strong><br>
+				<span class="muted">1.8.20</span>
+			</td>
+			<td>jQuery UI is a lightweight, easily customizable interface library for the jQuery Javascript library.</td>
+		</tr>
+		<tr>
+			<td>
+				<strong><a href="http://www.no-margin-for-errors.com/projects/prettyphoto-jquery-lightbox-clone/" target="_blank">prettyPhoto</a></strong><br>
+				<span class="muted">3.1.4</span>
+			</td>
+			<td>prettyPhoto is a jQuery lightbox clone. Not only does it support images, it also support for videos, flash, YouTube, iframes. It's a full blown media lightbox.</td>
+		</tr>
+		<tr>
+			<td>
+				<strong><a href="http://famspam.com/facebox" target="_blank">FaceBox</a></strong><br>
+				<span class="muted">1.2</span>
+			</td>
+			<td>Facebox is a jQuery-based lightbox which can display images, divs, or entire remote pages.</td>
+		</tr>
+		<tr>
+			<td>
+				<strong><a href="http://plugins.jquery.com/project/ajaxq" target="_blank">AjaxQ</a></strong>
+			</td>
+			<td>AjaxQ is a jQuery plugin that implements an AJAX request queueing mechanism.</td>
+		</tr>
+		<tr>
+			<td>
+				<strong><a href="http://www.unwrongest.com/projects/lazy/" target="_blank">Lazy</a></strong><br>
+				<span class="muted">1.5</span>
+			</td>
+			<td>Lazy is an on-demand jQuery plugin loader, also known as a lazy loader. Instead of downloading all jQuery plugins you might or might not need when the page loads, Lazy downloads the plugins when you actually use them. Lazy is very lightweight, super fast, and smart. Lazy will keep track of all your plugins and dependencies and make sure that they are only downloaded once.</td>
+		</tr>
+		<tr>
+			<td>
+				<strong><a href="http://www.digitalia.be/software/reflectionjs-for-jquery" target="_blank">jQuery Reflection</a></strong><br>
+				<span class="muted">1.1</span>
+			</td>
+			<td>This is an improved version of the reflection.js script rewritten for the jQuery Javascript library. It allows you to add instantaneous reflection effects to your images in modern browsers, in less than 2 KB.</td>
+		</tr>
+		<tr>
+			<td>
+				<strong><a href="http://rikrikrik.com/jquery/quicksearch/" target="_blank">QuickSearch</a></strong>
+			</td>
+			<td>QuickSearch is a simple plugin for filtering tables, lists and paragraphs.</td>
+		</tr>
+		<tr>
+			<td>
+				<strong><a href="http://markitup.jaysalvat.com/home/" target="_blank">markItUp!</a></strong><br>
+				<span class="muted">1.1.12</span>
+			</td>
+			<td>markItUp! is a JavaScript plugin built on the jQuery library that allows you to turn any textarea into a markup editor.</td>
+		</tr>
+		<tr>
+			<td>
+				<strong><a href="http://textpattern.com/download" target="_blank">Textile</a></strong><br>
+				<span class="muted">2.0.0</span>
+			</td>
+			<td>Textile is a lightweight markup language that converts its marked-up text input to valid, well-formed XHTML and also inserts character entity references for apostrophes, opening and closing single and double quotation marks, ellipses and em dashes.</td>
+		</tr>
+		<tr>
+			<td>
+				<strong><a href="http://michelf.com/projects/php-markdown/" target="_blank">PHP Markdown</a></strong><br>
+				<span class="muted">1.2.4</span>
+			</td>
+			<td>PHP Markdown is a port to PHP of the Markdown program written by John Gruber. Markdown is two things: a plain text markup syntax, and a software tool that converts the plain text markup to HTML for publishing on the web.</td>
+		</tr>
+		<tr>
+			<td>
+				<strong><a href="http://codeigniter.com/wiki/YAYparser/" target="_blank">YAYparser</a></strong>
+			</td>
+			<td>YAYparser is a regular expression driven YAML parser that is aimed to be small and easy to use.</td>
+		</tr>
+		<tr>
+			<td>
+				<strong><a href="http://davidwalsh.name/jquery-countdown-plugin" target="_blank">jQuery Countdown</a></strong>
+			</td>
+			<td>A simple plugin that counts down and updates the text every second.</td>
+		</tr>
+		<tr>
+			<td>
+				<strong><a href="http://github.com/pixelmatrix/uniform" target="_blank">Uniform</a></strong><br>
+				<span class="muted">1.7.5</span>
+			</td>
+			<td>Uniform masks your standard form controls with custom themed controls. It works in sync with your real form elements to ensure accessibility and compatibility.</td>
+		</tr>
+		<tr>
+			<td>
+				<strong><a href="http://harvesthq.github.com/chosen/" target="_blank">Chosen</a></strong>
+			</td>
+			<td>Chosen is a JavaScript plugin that makes long, unwieldy select boxes much more user-friendly.</td>
+		</tr>
+		<tr>
+			<td>
+				<strong><a href="http://twitter.github.com/bootstrap/" target="_blank">Bootstrap</a></strong><br>
+				<span class="muted">1.4</span>
+			</td>
+			<td>Bootstrap is a toolkit from Twitter designed to kickstart development of webapps and sites. It includes base CSS and HTML for typography, forms, buttons, tables, grids, navigation, and more.</td>
+		</tr>
+	</tbody>
+</table>
